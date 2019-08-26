277a559f5fe3163b128972cf580b314e23a2a98d
Joshua Vial <joshua@enspiral.com>
2019-08-26 23:37:26 +1200

    setting up webpack for client files

--- server/server.js
+++ server/server.js
@@ -7,10 +7,14 @@ const server = express()
 
 // Middleware
 
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main'
+}))
 server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
 server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
 
 // Routes
 
